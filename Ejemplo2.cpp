fec4eaee69d0a13d7878cb0923ffc7ef08cab67d
Winry4 <rebecamolinaprados@gmail.com>
2020-12-06 11:54:26 +0100

    Commit

--- Ejemplo2.cpp
+++ Ejemplo2.cpp
@@ -1,140 +1,83 @@
-void ActivationMessages::sendActivationMessage(uint8_t byte1, uint16_t idObject, uint8_t state, uint8_t aux)
+bool ControllerVT::tryTakeFreeDir()
 {
-    QList<uint8_t> message;
-
-    uint8_t sa = OPManage::getInstance()->list_object.at(OPManage::getInstance()->idContainerWS)->getName()->getSource_address();
-    switch(byte1)
+    if (timerAddress.elapsed() > (500 + ((qrand() % 256 + 0) * 0.6)))
     {
-        case 0:// Soft Key Activation Message
+        if (address[PREFERED_ADDRESS] == 0) //Try take prefered address if this is free
         {
-            message.append(byte1);                                          //Command
-            message.append(state);                                          // 0 released - 1 Pressed - 2 Still held - 3 Press aborted
-            message.append(idObject & 0x00FF);                              //ID Object
-            message.append((idObject & 0xFF00)>>8);                         //ID Object
-            message.append(OPManage::getInstance()->activeMask);            //CAMBIAR
-            message.append(OPManage::getInstance()->activeMask2);           //Key Group Object if the key is in a visible group
-            message.append(aux);                                            //Soft Key Code/ 0 = ACK Alarm
-            message.append(0xFF);
-            //message.append(0xF | TAN << 4);                               //VT Version 6 and later send TAN (Transaction Number)
-            //TAN++;
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
+            CommunicationController::getInstance()->setidVT(PREFERED_ADDRESS);
         }
-        case 1:// Button Activation Message
+        else if (self_configurable_address == 0x01) //if we are configurable device, try take a free direction
         {
-            message.append(byte1);                                          //Command
-            message.append(state);                                          // 0 unlatched or released - 1 Pressed or latched - 2 Still held - 3 Press aborted
-            message.append(idObject & 0x00FF);                              //ID Object
-            message.append((idObject & 0xFF00)>>8);                         //ID Object
-            message.append(OPManage::getInstance()->activeMask);            //CAMBIAR
-            message.append(OPManage::getInstance()->activeMask2);           //Or Window Mask object
-            message.append(aux);                                            //Button Key Code
-            message.append(0xFF);
-            //message.append(0xF | TAN << 4);                               //VT Version 6 and later send TAN (Transaction Number)
-            //TAN++;
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
+            for (uint8_t i = 128L; i < 248L; i++)
+            {
+                if (address[i] != 1)
+                {
+                    CommunicationController::getInstance()->setidVT(i);
+                    i = 250;
+                }
+            }
         }
-        case 3://Select Input Object
+        if ((CommunicationController::getInstance()->getidVT() == 0xFE)) //If there aren´t a free directions try to steal it from another device
         {
-            message.append(byte1);                                          //Command
-            message.append(idObject & 0x00FF);                              //ID Object
-            message.append((idObject & 0xFF00)>>8);                         //ID Object
-            message.append(state);                                          // 0 deselected - 1 selected
-            message.append(aux);                                            //Bitmask. (Bit 0=1 Object is open for data input)
-            message.append(0xFF);                                           //Reserved
-            message.append(0xFF);                                           //Reserved
-            message.append(0xFF);
-            //message.append(0xF | TAN << 4);                               //VT Version 6 and later send TAN (Transaction Number)
-            //TAN++;
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
+            if (tryTakeDirByPriority() == false) // If we don´t have more priority than any device, we wait 8s and repeat process
+            {
+                this->sleep(3);
+                askAddress();
+                std::fill(address, address + 255, 0); //reset vector
+                nameList.clear();                     //reset NAME table
+                timerAddress.restart();               // reset timer
+                return false;
+            }
+            return true;
         }
-        case 4:// ESC message
-        {
-            message.append(byte1);                                          //Command
-            message.append(idObject & 0x00FF);                              //ID Object
-            message.append((idObject & 0xFF00)>>8);                         //ID Object
-            message.append(state);                                          //Error Codes
-            message.append(0xFF);
-            message.append(0xFF);
-            message.append(0xFF);
-            message.append(0xFF);
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
-        }
-        case 6:// Change Active Mask message
-        {
-            message.append(byte1);                                          //Command
-            message.append(OPManage::getInstance()->activeMask);            //Mask Object ID
-            message.append(OPManage::getInstance()->activeMask2);           //Mask Object ID
-            message.append(state);                                          //Error Codes
-            message.append(idObject & 0x00FF);                              //ID Object
-            message.append((idObject & 0xFF00)>>8);                         //ID Object
-            message.append(0xFF);
-            message.append(0xFF);
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
-        }
-        case 7:// Change Soft Key Mask message
-        {
-            message.append(byte1);                                          //Command
-            message.append(OPManage::getInstance()->activeMask);            //Mask Object ID
-            message.append(OPManage::getInstance()->activeMask2);           //Mask Object ID
-            message.append(OPManage::getInstance()->activeSoftKey);         //Soft Key Mask ID
-            message.append(OPManage::getInstance()->activeSoftKey2);        //Soft Key Mask ID
-            message.append(state);                                          //Error Codes
-            message.append(0xFF);
-            message.append(0xFF);
-            CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-            MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-            sendMessagesList.append(m);
-            break;
-        }
-        case 0x09:// On User-Layout Hide/Show message
-        {
-            break;
-        }
-        case 0x0A:// Control Audio Signal Termination message
-        {
-            break;
-        }
+        return true;
     }
-
+    return false;
+}
+void ControllerVT::queuedMessages(QList<uint8_t> message_list)
+{
+    this->freePositionSend.acquire();
+    sendMessagesBufferVT[writeBufferPosSend % INPUT_BUFFER_SIZE] = message_list;
+    writeBufferPosSend++;
+    this->usedPositionSend.release();
 }
 /**************************************************************************************************************************/
 /**
- * @brief ActivationMessages::sendActivationMessage. Pointing Event message
+ * @brief ControllerVT::findSourceAddress. Devuelve la posicion del NAME en la lista de nombres
+ * @param name
+ * @return
  */
-void ActivationMessages::sendActivationMessage(uint8_t state, uint16_t x, uint16_t y)
+uint8_t ControllerVT::findSourceAddress(Name *name)
 {
-    Q_UNUSED(state)
-    QList<uint8_t> message;
-    uint8_t sa = OPManage::getInstance()->list_object.at(OPManage::getInstance()->idContainerWS)->getName()->getSource_address();
-
-    message.append(0x02);                                           //Command
-    message.append((x & 0x00FF));                                   //X Position
-    message.append((x & 0xFF00)>>8);                                //X Position
-    message.append((y & 0x00FF));                                   //Y Position
-    message.append((y & 0xFF00)>>8);                                //Y Position
-    //message.append(state);                                          //0 released - 1 Pressed - 2 held
-    message.append(0xFF);
-    message.append(0xFF);
-    message.append(0xFF);
-
-//    message.append(OPManage::getInstance()->activeMask);            //VT 6 and later Parent Mask ID
-//    message.append(OPManage::getInstance()->activeMask2);
-    CommunicationController::getInstance()->sendMessage(message, 0xE600, sa, true);
-    MessagesToSend *m = new MessagesToSend(message, sa, 0xE600);
-    sendMessagesList.append(m);
+    for (int i = 0; i < this->nameList.size(); i++)
+    {
+        if (nameList.at(i)->comparator(name)) //Are equal
+        {
+            return nameList.at(i)->getSource_address();
+        }
+    }
+    return 0xFE;
+}
+/**************************************************************************************************************************/
+/**
+ * @brief ControllerVT::tryTakeDirByPriority. Funcion para tomar la direccion de otro dispositivo si el VT tiene mas prioridad
+ * @return
+ */
+bool ControllerVT::tryTakeDirByPriority()
+{
+    float64_t myName = calculateMyPriority();
+    for (int i = 0; i < nameList.size(); i++)
+    {
+        if (nameList.at(i)->getSource_address() != PREFERED_ADDRESS && nameList.at(i)->getSource_address() > 127 && nameList.at(i)->getSource_address() < 247)
+        {
+            float64_t deviceName = static_cast<unsigned long long>(nameList.at(i)->getbyte8()) | static_cast<unsigned long long>(nameList.at(i)->getbyte7()) << 8 | static_cast<unsigned long long>(nameList.at(i)->getbyte6()) << 16 | static_cast<unsigned long long>(nameList.at(i)->getbyte5()) << 24 | static_cast<unsigned long long>(nameList.at(i)->getbyte4()) << 32 | static_cast<unsigned long long>(nameList.at(i)->getbyte3()) << 40 | static_cast<unsigned long long>(nameList.at(i)->getbyte2()) << 48 | static_cast<unsigned long long>(nameList.at(i)->getbyte1()) << 56;
+            if (myName < deviceName) //VT have more priority
+            {
+                CommunicationController::getInstance()->setidVT(nameList.at(i)->getSource_address()); //Send address
+                i = nameList.size();
+                return true;
+            }
+        }
+    }
+    return false;
 }
